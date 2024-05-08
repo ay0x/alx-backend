84f2c875f7c21dd8bc3329dde662fa1125ab9f00
ay0x <85720179+ay0x@users.noreply.github.com>
2024-05-08 20:35:25 +0200

    fixed 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,35 +8,27 @@ class FIFOCache(BaseCaching):
         put(key, item) - store a key-value pair
         get(key) - retrieve the value associated with a key
     """
-
     def __init__(self):
         """
         Initialize the class using the parent class __init__ method
         """
         super().__init__()
-        self.queue = []
 
     def put(self, key, item):
-        """
-        Store a key-value pair
-        Args:
-            key: Key
-            item: Item
-        """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.queue.remove(key)
-            elif len(self.cache_data) >= self.MAX_ITEMS:
-                removed_key = self.queue.pop(0)
-                del self.cache_data[removed_key]
-            self.cache_data[key] = item
-            self.queue.append(key)
+        """Store a key-value pair"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            # Get the first key entered (FIFO)
+            first_key = next(iter(self.cache_data))
+            print("DISCARD:", first_key)
+            del self.cache_data[first_key]
+
+        self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Return value linked to key.
-        If key is None or doesn't exist, return None
-        """
-        if key is not None and key in self.cache_data.keys():
+        """Return value linked to key"""
+        if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
