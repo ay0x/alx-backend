a63c7a17e54f3121be9f4327edefd7cf9e9bf24e
ay0x <85720179+ay0x@users.noreply.github.com>
2024-05-08 22:53:17 +0200

    2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-""" BaseCaching module"""
+""" BaseCaching module
+"""
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """
-    Defines a class for caching information using LIFO strategy
-    Methods:
-        put(key, item) - store a key-value pair
-        get(key) - retrieve the value associated with a key
+    FIFOCache defines a FIFO caching system
     """
+
     def __init__(self):
         """
-        Initialize the class using the parent class __init__ method
+        Initialize the class with the parent's init method
         """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        """Store a key-value pair"""
+        """
+        Cache a key-value pair
+        """
         if key is None or item is None:
             pass
         else:
-            if len(self.cache_data) >= self.MAX_ITEMS and\
-              key not in self.cache_data:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
                 print("DISCARD: {}".format(self.order[-1]))
                 del self.cache_data[self.order[-1]]
                 del self.order[-1]
@@ -33,7 +34,9 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Return value linked to key"""
+        """
+        Return the value linked to a given key, or None
+        """
         if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
         return None
